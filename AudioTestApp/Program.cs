019de699d3291e7fe5eaea47e94f22e2b88db802
Michael Oborne <mich146@hotmail.com>
2020-10-04 19:25:29 +0800

    audio send test

--- AudioTestApp/Program.cs
+++ AudioTestApp/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Tasks;
 using NAudio.Wave;
 
 namespace AudioTestApp
@@ -12,9 +13,22 @@ namespace AudioTestApp
     {
         static void Main(string[] args)
         {
+            var endpoint = new IPEndPoint(IPAddress.Parse("10.1.1.107"), 5060);
             var listener = new UdpClient(5060);
 
             int SampleRate = 8000;
+
+            var wavein = new WaveInEvent();
+            wavein.DeviceNumber = 0;
+            wavein.WaveFormat = new WaveFormat(SampleRate, 16, 1);
+
+            wavein.StartRecording();
+
+            wavein.DataAvailable += (sender, eventArgs) =>
+            {
+                listener.Send(eventArgs.Buffer, eventArgs.BytesRecorded, endpoint);
+            };
+
 
             BufferedWaveProvider bufferedWave = new BufferedWaveProvider(new WaveFormat(SampleRate, 16, 1));
             bufferedWave.BufferDuration = TimeSpan.FromMilliseconds(200);
